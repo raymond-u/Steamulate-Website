068aa035d2637003edef18abbcf98916c7562a8b
Raymond Underwood <raymondunderwood.001@gmail.com>
2019-02-21 19:17:15 +0900

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+(function () {
+	var navbar = document.getElementById("navbar");
+    
+	function changeNavbar(force = false) {
+		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+		navbar.classList.toggle("nav-light", scrollTop >= 1 || force);
+	}
+    
+    document.getElementById("a-dropdown").addEventListener("click", function (event) {
+		changeNavbar(true);
+	});
+    
+    document.getElementById("button-collapse").addEventListener("click", function (event) {
+		changeNavbar(true);
+	});
+    
+	var didScroll = false;
+    
+	$(window).scroll(function () {
+		didScroll = true;
+	});
+    
+	setInterval(function() {
+		if (didScroll) {
+			didScroll = false;
+			changeNavbar();
+		}
+	}, 100);
+    
+	changeNavbar();
+	
+	$("a[href*=\\#]").on("click", function (event) {
+		if(this.pathname === window.location.pathname) {
+			event.preventDefault();
+
+	        $("html, body").animate({
+			    scrollTop: $(this.hash).offset().top
+			}, 500);
+		}
+	});
+})();
